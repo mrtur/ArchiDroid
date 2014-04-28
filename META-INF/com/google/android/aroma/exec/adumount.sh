JustArchi/ArchiDroid-legacy

Code review, part one

META-INF/com/google/android/aroma/exec/adumount.sh
@@ -25,67 +25,61 @@
 # Usage: adumount.sh *path*, f.e. adumount.sh /storage/sdcard1
 
 # These are absolute paths without slashes, for example /storage/sdcard1 is storagesdcard1, because you can't use / in variables
-efs="/dev/block/mmcblk0p3" # EFS, if available
-boot="/dev/block/mmcblk0p5" # ROM's kernel image
-recovery="/dev/block/mmcblk0p6" # Recovery image
-radio="/dev/block/mmcblk0p7" # Modem image
-cache="/dev/block/mmcblk0p8" # Cache partition
-system="/dev/block/mmcblk0p9" # System partition
-preload="/dev/block/mmcblk0p10" # Preload partition (also SELinux)
-data="/dev/block/mmcblk0p12" # Data and internal memory
-storagesdcard1="/dev/block/mmcblk1p1" # External memory, if available
-AUTO="/efs /system /cache /preload /data /storage/sdcard1" # Filesystems which should be unmounted automatically when no argument is given, typically every partition excluding images
+fs="ext4" # Filesystem
+efs="/dev/block/mmcblk0p3" # EFS, if available, used by eval in stages 2+, SC2034
+cache="/dev/block/mmcblk0p8" # Cache partition, used by eval in stages 2+, SC2034
+system="/dev/block/mmcblk0p9" # System partition, used by eval in stages 2+, SC2034
+preload="/dev/block/mmcblk0p10" # Preload partition (also SELinux), used by eval in stages 2+, SC2034
+data="/dev/block/mmcblk0p12" # Data and internal memory, used by eval in stages 2+, SC2034
+storagesdcard1="/dev/block/mmcblk1p1" # External memory, if available, used by eval in stages 2+, SC2034
+AUTO="/efs /cache /system /preload /data /storage/sdcard1" # Filesystems which should be unmounted automatically when no argument is given, typically every partition excluding images
 
-GOTBUSYBOX=false
-GOTMOUNT=false
-LOG="/tmp/archidroid_mount.log" # We can use /dev/null if not required
+GOTBUSYBOX="false"
+GOTMOUNT="false"
+LOG="/dev/null" # We can use /dev/null if not required
 
 ADMOUNTED() {
-	if [ $(mount | grep -i "$1" | wc -l) -gt 0 ]; then
-		return 0
-	else
-		return 1
-	fi
+	return "$(mount | grep -qi "$1"; echo $?)"
 }
 
 ADUMOUNT() {
-	if (ADMOUNTED "$1"); then
-		MNTPATH=$(echo $1 | sed 's/\///g')
+	if ADMOUNTED "$1"; then
+		MNTPATH="$(echo "$1" | sed 's/\///g')"
 		eval "MNTPATH=\$$MNTPATH"
-		if $GOTBUSYBOX; then
+		if "$GOTBUSYBOX"; then
 			busybox umount -f "$1" >/dev/null 2>&1
 			busybox umount -f "$MNTPATH" >/dev/null 2>&1 # This is required for freeing up block path completely, used for example in reformatting
-			if !(ADMOUNTED "$1"); then
-				echo "Successfully unmounted $1 through busybox umount" >> $LOG
+			if ! ADMOUNTED "$1"; then
+				echo "Successfully unmounted $1 through busybox umount" >> "$LOG"
 				return 0
 			fi
 		fi
-		if $GOTMOUNT; then
+		if "$GOTMOUNT"; then
 			umount -f "$1" >/dev/null 2>&1
 			umount -f "$MNTPATH" >/dev/null 2>&1 # This is required for freeing up block path completely, used for example in reformatting
-			if !(ADMOUNTED "$1"); then
-				echo "Successfully unmounted $1 through umount" >> $LOG
+			if ! ADMOUNTED "$1"; then
+				echo "Successfully unmounted $1 through umount" >> "$LOG"
 				return 0
 			fi
 		fi
 		# Ok, I give up
-		echo "ERROR: Could not unmount $1" >> $LOG
+		echo "ERROR: Could not unmount $1" >> "$LOG"
 		return 1
 	else
-		echo "$1 is already unmounted" >> $LOG
+		echo "$1 is already unmounted" >> "$LOG"
 	fi
 	return 0
 }
 
-if [ ! -z $(which busybox) ]; then
-	GOTBUSYBOX=true
+if [ ! -z "$(which busybox)" ]; then
+	GOTBUSYBOX="true"
 fi
-if [ ! -z $(which mount) ]; then
-	GOTMOUNT=true
+if [ ! -z "$(which mount)" ]; then
+	GOTMOUNT="true"
 fi
-if (! $GOTBUSYBOX && ! $GOTMOUNT); then
+if ! "$GOTBUSYBOX" && ! "$GOTMOUNT"; then
 	# This should never happen, but safety check is always good
-	echo "FATAL ERROR, NO BUSYBOX NEITHER MOUNT" >> $LOG
+	echo "FATAL ERROR, NO BUSYBOX NEITHER MOUNT" >> "$LOG"
 	exit 1
 fi
 

META-INF/com/google/android/aroma/exec/archidroid_check_fs.sh
@@ -22,7 +22,7 @@
 # limitations under the License.
 
 if [ $# -eq 0 ]; then
-	if [ -z $(which mkfs.f2fs) ]; then
+	if [ -z "$(which mkfs.f2fs)" ]; then
 		exit 1
 	else
 		exit 0
@@ -31,107 +31,100 @@ fi
 
 # These are absolute paths without slashes, for example /storage/sdcard1 is storagesdcard1, because you can't use / in variables
 fs="ext4" # Filesystem
-efs="/dev/block/mmcblk0p3" # EFS, if available
-boot="/dev/block/mmcblk0p5" # ROM's kernel image
-recovery="/dev/block/mmcblk0p6" # Recovery image
-radio="/dev/block/mmcblk0p7" # Modem image
-cache="/dev/block/mmcblk0p8" # Cache partition
-system="/dev/block/mmcblk0p9" # System partition
-preload="/dev/block/mmcblk0p10" # Preload partition (also SELinux)
-data="/dev/block/mmcblk0p12" # Data and internal memory
-storagesdcard1="/dev/block/mmcblk1p1" # External memory, if available
+efs="/dev/block/mmcblk0p3" # EFS, if available, used by eval in stages 2+, SC2034
+cache="/dev/block/mmcblk0p8" # Cache partition, used by eval in stages 2+, SC2034
+system="/dev/block/mmcblk0p9" # System partition, used by eval in stages 2+, SC2034
+preload="/dev/block/mmcblk0p10" # Preload partition (also SELinux), used by eval in stages 2+, SC2034
+data="/dev/block/mmcblk0p12" # Data and internal memory, used by eval in stages 2+, SC2034
+storagesdcard1="/dev/block/mmcblk1p1" # External memory, if available, used by eval in stages 2+, SC2034
 
-GOTBUSYBOX=false
-GOTMOUNT=false
+GOTBUSYBOX="false"
+GOTMOUNT="false"
 LOG="/dev/null" # We can use /dev/null if not required
 
 ADMOUNTED() {
-	if [ $(mount | grep -i "$1" | wc -l) -gt 0 ]; then
-		return 0
-	else
-		return 1
-	fi
+	return "$(mount | grep -qi "$1"; echo $?)"
 }
 
 ADMOUNT() {
-	if !(ADMOUNTED "$1"); then
+	if ! ADMOUNTED "$1"; then
 		mkdir -p "$1"
 		# Stage 1, let's use automatic filesystem and block path, as recovery should have valid fstab file. This will be enough in most scenarios.
-		if $GOTBUSYBOX; then
+		if "$GOTBUSYBOX"; then
 			busybox mount -t auto "$1" >/dev/null 2>&1
-			if (ADMOUNTED "$1"); then
-				echo "Stage 1: Successfully mounted $1 through busybox mount" >> $LOG
+			if ADMOUNTED "$1"; then
+				echo "Stage 1: Successfully mounted $1 through busybox mount" >> "$LOG"
 				return 0
 			fi
 		fi
-		if $GOTMOUNT; then
+		if "$GOTMOUNT"; then
 			mount -t auto "$1" >/dev/null 2>&1
-			if (ADMOUNTED "$1"); then
-				echo "Stage 1: Successfully mounted $1 through mount" >> $LOG
+			if ADMOUNTED "$1"; then
+				echo "Stage 1: Successfully mounted $1 through mount" >> "$LOG"
 				return 0
 			fi
 		fi
 		# Stage 2, mounted device isn't available in fstab and/or recovery can't mount it without such information. This is typical for f2fs, as fstab has ext4 declared. In addition to Stage 1, we'll provide block path, this should be enough.
-		if $GOTBUSYBOX; then
-			MNTPATH=$(echo $1 | sed 's/\///g')
-			eval "MNTPATH=\$$MNTPATH"
+		if "$GOTBUSYBOX"; then
+			MNTPATH="$(echo "$1" | sed 's/\///g')"
+			eval "MNTPATH=\$$MNTPATH" # EVAL IS EVIL
 			busybox mount -t auto "$MNTPATH" "$1" >/dev/null 2>&1
-			if (ADMOUNTED "$1"); then
-				echo "Stage 2: Successfully mounted $1 through busybox mount and $MNTPATH" >> $LOG
+			if ADMOUNTED "$1"; then
+				echo "Stage 2: Successfully mounted $1 through busybox mount and $MNTPATH" >> "$LOG"
 				return 0
 			fi
 		fi
-		if $GOTMOUNT; then
-			MNTPATH=$(echo $1 | sed 's/\///g')
-			eval "MNTPATH=\$$MNTPATH"
-			mount -t $auto "$MNTPATH" "$1" >/dev/null 2>&1
-			if (ADMOUNTED "$1"); then
-				echo "Stage 2: Successfully mounted $1 through mount and $MNTPATH" >> $LOG
+		if "$GOTMOUNT"; then
+			MNTPATH="$(echo "$1" | sed 's/\///g')"
+			eval "MNTPATH=\$$MNTPATH" # EVAL IS EVIL
+			mount -t auto "$MNTPATH" "$1" >/dev/null 2>&1
+			if ADMOUNTED "$1"; then
+				echo "Stage 2: Successfully mounted $1 through mount and $MNTPATH" >> "$LOG"
 				return 0
 			fi
 		fi
 		# Stage 3, we failed using automatic filesystem, so we'll now use full mount command. This is our last chance.
-		if $GOTBUSYBOX; then
-			MNTPATH=$(echo $1 | sed 's/\///g')
-			eval "MNTPATH=\$$MNTPATH"
+		if "$GOTBUSYBOX"; then
+			MNTPATH="$(echo "$1" | sed 's/\///g')"
+			eval "MNTPATH=\$$MNTPATH" # EVAL IS EVIL
 			busybox mount -t "$fs" "$MNTPATH" "$1" >/dev/null 2>&1
-			if (ADMOUNTED "$1"); then
-				echo "Stage 3: Successfully mounted $1 through busybox mount, using $fs filesystem and $MNTPATH" >> $LOG
+			if ADMOUNTED "$1"; then
+				echo "Stage 3: Successfully mounted $1 through busybox mount, using $fs filesystem and $MNTPATH" >> "$LOG"
 				return 0
 			fi
 		fi
-		if $GOTMOUNT; then
-			MNTPATH=$(echo $1 | sed 's/\///g')
-			eval "MNTPATH=\$$MNTPATH"
+		if "$GOTMOUNT"; then
+			MNTPATH="$(echo "$1" | sed 's/\///g')"
+			eval "MNTPATH=\$$MNTPATH" # EVAL IS EVIL
 			mount -t "$fs" "$MNTPATH" "$1" >/dev/null 2>&1
-			if (ADMOUNTED "$1"); then
-				echo "Stage 3: Successfully mounted $1 through mount, using $fs filesystem and $MNTPATH" >> $LOG
+			if ADMOUNTED "$1"; then
+				echo "Stage 3: Successfully mounted $1 through mount, using $fs filesystem and $MNTPATH" >> "$LOG"
 				return 0
 			fi
 		fi
 		# Stage 4, we're out of ideas
-		echo "Stage 4: ERROR! Could not mount $1" >> $LOG
+		echo "Stage 4: ERROR! Could not mount $1" >> "$LOG"
 		return 1
 	else
-		echo "$1 is already mounted" >> $LOG
+		echo "$1 is already mounted" >> "$LOG"
 	fi
 	return 0
 }
 
-if [ ! -z $(which busybox) ]; then
-	GOTBUSYBOX=true
+if [ ! -z "$(which busybox)" ]; then
+	GOTBUSYBOX="true"
 fi
-if [ ! -z $(which mount) ]; then
-	GOTMOUNT=true
+if [ ! -z "$(which mount)" ]; then
+	GOTMOUNT="true"
 fi
-if (! $GOTBUSYBOX && ! $GOTMOUNT); then
+if ! "$GOTBUSYBOX" && ! "$GOTMOUNT"; then
 	# This should never happen, but safety check is always good
-	echo "FATAL ERROR, NO BUSYBOX NEITHER MOUNT" >> $LOG
+	echo "FATAL ERROR, NO BUSYBOX NEITHER MOUNT" >> "$LOG"
 	exit 1
 fi
 
 ADMOUNT "$1"
-FS=$(mount | grep "$1" | head -n 1 | awk '{print $5}')
+FS="$(mount | grep "$1" | head -n 1 | awk '{print $5}')"
 echo "$FS"
 
 sync

mrtur/ArchiDroid

META-INF/com/google/android/aroma/exec/adumount.sh
@@ -25,64 +25,61 @@
 # Usage: adumount.sh *path*, f.e. adumount.sh /storage/sdcard1
 
 # These are absolute paths without slashes, for example /storage/sdcard1 is storagesdcard1, because you can't use / in variables
-boot="/dev/block/mmcblk0p6" # ROM's kernel image
-recovery="/dev/block/mmcblk0p7" # Recovery image
-radio="/dev/block/mmcblk0p1" # Modem image
-cache="/dev/block/mmcblk0p22" # Cache partition
-system="/dev/block/mmcblk0p21" # System partition
-data="/dev/block/mmcblk0p23" # Data and internal memory
+fs="ext4" # Filesystem, used by eval in stages 2+, SC2034
+boot="/dev/block/mmcblk0p6" # ROM's kernel image, used by eval in stages 2+, SC2034
+recovery="/dev/block/mmcblk0p7" # Recovery image, used by eval in stages 2+, SC2034
+radio="/dev/block/mmcblk0p1" # Modem image, used by eval in stages 2+, SC2034
+cache="/dev/block/mmcblk0p22" # Cache partition, used by eval in stages 2+, SC2034
+system="/dev/block/mmcblk0p21" # # System partition, used by eval in stages 2+, SC2034
+data="/dev/block/mmcblk0p23" # Data and internal memory, used by eval in stages 2+, SC2034
 AUTO="/system /cache /data" # Filesystems which should be unmounted automatically when no argument is given, typically every partition excluding images
 
-GOTBUSYBOX=false
-GOTMOUNT=false
-LOG="/tmp/mrturdroid_mount.log" # We can use /dev/null if not required
+GOTBUSYBOX="false"
+GOTMOUNT="false"
+LOG="/dev/null" # We can use /dev/null if not required
 
 ADMOUNTED() {
-	if [ $(mount | grep -i "$1" | wc -l) -gt 0 ]; then
-		return 0
-	else
-		return 1
-	fi
+	return "$(mount | grep -qi "$1"; echo $?)"
 }
 
 ADUMOUNT() {
-	if (ADMOUNTED "$1"); then
-		MNTPATH=$(echo $1 | sed 's/\///g')
+	if ADMOUNTED "$1"; then
+		MNTPATH="$(echo "$1" | sed 's/\///g')"
 		eval "MNTPATH=\$$MNTPATH"
-		if $GOTBUSYBOX; then
+		if "$GOTBUSYBOX"; then
 			busybox umount -f "$1" >/dev/null 2>&1
 			busybox umount -f "$MNTPATH" >/dev/null 2>&1 # This is required for freeing up block path completely, used for example in reformatting
-			if !(ADMOUNTED "$1"); then
-				echo "Successfully unmounted $1 through busybox umount" >> $LOG
+			if ! ADMOUNTED "$1"; then
+				echo "Successfully unmounted $1 through busybox umount" >> "$LOG"
 				return 0
 			fi
 		fi
-		if $GOTMOUNT; then
+		if "$GOTMOUNT"; then
 			umount -f "$1" >/dev/null 2>&1
 			umount -f "$MNTPATH" >/dev/null 2>&1 # This is required for freeing up block path completely, used for example in reformatting
-			if !(ADMOUNTED "$1"); then
-				echo "Successfully unmounted $1 through umount" >> $LOG
+			if ! ADMOUNTED "$1"; then
+				echo "Successfully unmounted $1 through umount" >> "$LOG"
 				return 0
 			fi
 		fi
 		# Ok, I give up
-		echo "ERROR: Could not unmount $1" >> $LOG
+		echo "ERROR: Could not unmount $1" >> "$LOG"
 		return 1
 	else
-		echo "$1 is already unmounted" >> $LOG
+		echo "$1 is already unmounted" >> "$LOG"
 	fi
 	return 0
 }
 
-if [ ! -z $(which busybox) ]; then
-	GOTBUSYBOX=true
+if [ ! -z "$(which busybox)" ]; then
+	GOTBUSYBOX="true"
 fi
-if [ ! -z $(which mount) ]; then
-	GOTMOUNT=true
+if [ ! -z "$(which mount)" ]; then
+	GOTMOUNT="true"
 fi
-if (! $GOTBUSYBOX && ! $GOTMOUNT); then
+if ! "$GOTBUSYBOX" && ! "$GOTMOUNT"; then
 	# This should never happen, but safety check is always good
-	echo "FATAL ERROR, NO BUSYBOX NEITHER MOUNT" >> $LOG
+	echo "FATAL ERROR, NO BUSYBOX NEITHER MOUNT" >> "$LOG"
 	exit 1
 fi
 

META-INF/com/google/android/aroma/exec/archidroid_check_fs.sh
@@ -22,7 +22,7 @@
 # limitations under the License.
 
 if [ $# -eq 0 ]; then
-	if [ -z $(which mkfs.f2fs) ]; then
+	if [ -z "$(which mkfs.f2fs)" ]; then
 		exit 1
 	else
 		exit 0
@@ -30,105 +30,102 @@ if [ $# -eq 0 ]; then
 fi
 
 # These are absolute paths without slashes, for example /storage/sdcard1 is storagesdcard1, because you can't use / in variables
-fs="ext4" # Filesystem
-boot="/dev/block/mmcblk0p6" # ROM's kernel image
-recovery="/dev/block/mmcblk0p7" # Recovery image
-radio="/dev/block/mmcblk0p1" # Modem image
-cache="/dev/block/mmcblk0p22" # Cache partition
-system="/dev/block/mmcblk0p21" # System partition
-data="/dev/block/mmcblk0p23" # Data and internal memory
+fs="ext4" # Filesystem, used by eval in stages 2+, SC2034
+boot="/dev/block/mmcblk0p6" # ROM's kernel image, used by eval in stages 2+, SC2034
+recovery="/dev/block/mmcblk0p7" # Recovery image, used by eval in stages 2+, SC2034
+radio="/dev/block/mmcblk0p1" # Modem image, used by eval in stages 2+, SC2034
+cache="/dev/block/mmcblk0p22" # Cache partition, used by eval in stages 2+, SC2034
+system="/dev/block/mmcblk0p21" # # System partition, used by eval in stages 2+, SC2034
+data="/dev/block/mmcblk0p23" # Data and internal memory, used by eval in stages 2+, SC2034
+AUTO="/system /cache /data" # Filesystems which should be unmounted automatically when no argument is given, typically every partition excluding images
 
-GOTBUSYBOX=false
-GOTMOUNT=false
+GOTBUSYBOX="false"
+GOTMOUNT="false"
 LOG="/dev/null" # We can use /dev/null if not required
 
 ADMOUNTED() {
-	if [ $(mount | grep -i "$1" | wc -l) -gt 0 ]; then
-		return 0
-	else
-		return 1
-	fi
+	return "$(mount | grep -qi "$1"; echo $?)"
 }
 
 ADMOUNT() {
-	if !(ADMOUNTED "$1"); then
+	if ! ADMOUNTED "$1"; then
 		mkdir -p "$1"
 		# Stage 1, let's use automatic filesystem and block path, as recovery should have valid fstab file. This will be enough in most scenarios.
-		if $GOTBUSYBOX; then
+		if "$GOTBUSYBOX"; then
 			busybox mount -t auto "$1" >/dev/null 2>&1
-			if (ADMOUNTED "$1"); then
-				echo "Stage 1: Successfully mounted $1 through busybox mount" >> $LOG
+			if ADMOUNTED "$1"; then
+				echo "Stage 1: Successfully mounted $1 through busybox mount" >> "$LOG"
 				return 0
 			fi
 		fi
-		if $GOTMOUNT; then
+		if "$GOTMOUNT"; then
 			mount -t auto "$1" >/dev/null 2>&1
-			if (ADMOUNTED "$1"); then
-				echo "Stage 1: Successfully mounted $1 through mount" >> $LOG
+			if ADMOUNTED "$1"; then
+				echo "Stage 1: Successfully mounted $1 through mount" >> "$LOG"
 				return 0
 			fi
 		fi
 		# Stage 2, mounted device isn't available in fstab and/or recovery can't mount it without such information. This is typical for f2fs, as fstab has ext4 declared. In addition to Stage 1, we'll provide block path, this should be enough.
-		if $GOTBUSYBOX; then
-			MNTPATH=$(echo $1 | sed 's/\///g')
-			eval "MNTPATH=\$$MNTPATH"
+		if "$GOTBUSYBOX"; then
+			MNTPATH="$(echo "$1" | sed 's/\///g')"
+			eval "MNTPATH=\$$MNTPATH" # EVAL IS EVIL
 			busybox mount -t auto "$MNTPATH" "$1" >/dev/null 2>&1
-			if (ADMOUNTED "$1"); then
-				echo "Stage 2: Successfully mounted $1 through busybox mount and $MNTPATH" >> $LOG
+			if ADMOUNTED "$1"; then
+				echo "Stage 2: Successfully mounted $1 through busybox mount and $MNTPATH" >> "$LOG"
 				return 0
 			fi
 		fi
-		if $GOTMOUNT; then
-			MNTPATH=$(echo $1 | sed 's/\///g')
-			eval "MNTPATH=\$$MNTPATH"
-			mount -t $auto "$MNTPATH" "$1" >/dev/null 2>&1
-			if (ADMOUNTED "$1"); then
-				echo "Stage 2: Successfully mounted $1 through mount and $MNTPATH" >> $LOG
+		if "$GOTMOUNT"; then
+			MNTPATH="$(echo "$1" | sed 's/\///g')"
+			eval "MNTPATH=\$$MNTPATH" # EVAL IS EVIL
+			mount -t auto "$MNTPATH" "$1" >/dev/null 2>&1
+			if ADMOUNTED "$1"; then
+				echo "Stage 2: Successfully mounted $1 through mount and $MNTPATH" >> "$LOG"
 				return 0
 			fi
 		fi
 		# Stage 3, we failed using automatic filesystem, so we'll now use full mount command. This is our last chance.
-		if $GOTBUSYBOX; then
-			MNTPATH=$(echo $1 | sed 's/\///g')
-			eval "MNTPATH=\$$MNTPATH"
+		if "$GOTBUSYBOX"; then
+			MNTPATH="$(echo "$1" | sed 's/\///g')"
+			eval "MNTPATH=\$$MNTPATH" # EVAL IS EVIL
 			busybox mount -t "$fs" "$MNTPATH" "$1" >/dev/null 2>&1
-			if (ADMOUNTED "$1"); then
-				echo "Stage 3: Successfully mounted $1 through busybox mount, using $fs filesystem and $MNTPATH" >> $LOG
+			if ADMOUNTED "$1"; then
+				echo "Stage 3: Successfully mounted $1 through busybox mount, using $fs filesystem and $MNTPATH" >> "$LOG"
 				return 0
 			fi
 		fi
-		if $GOTMOUNT; then
-			MNTPATH=$(echo $1 | sed 's/\///g')
-			eval "MNTPATH=\$$MNTPATH"
+		if "$GOTMOUNT"; then
+			MNTPATH="$(echo "$1" | sed 's/\///g')"
+			eval "MNTPATH=\$$MNTPATH" # EVAL IS EVIL
 			mount -t "$fs" "$MNTPATH" "$1" >/dev/null 2>&1
-			if (ADMOUNTED "$1"); then
-				echo "Stage 3: Successfully mounted $1 through mount, using $fs filesystem and $MNTPATH" >> $LOG
+			if ADMOUNTED "$1"; then
+				echo "Stage 3: Successfully mounted $1 through mount, using $fs filesystem and $MNTPATH" >> "$LOG"
 				return 0
 			fi
 		fi
 		# Stage 4, we're out of ideas
-		echo "Stage 4: ERROR! Could not mount $1" >> $LOG
+		echo "Stage 4: ERROR! Could not mount $1" >> "$LOG"
 		return 1
 	else
-		echo "$1 is already mounted" >> $LOG
+		echo "$1 is already mounted" >> "$LOG"
 	fi
 	return 0
 }
 
-if [ ! -z $(which busybox) ]; then
-	GOTBUSYBOX=true
+if [ ! -z "$(which busybox)" ]; then
+	GOTBUSYBOX="true"
 fi
-if [ ! -z $(which mount) ]; then
-	GOTMOUNT=true
+if [ ! -z "$(which mount)" ]; then
+	GOTMOUNT="true"
 fi
-if (! $GOTBUSYBOX && ! $GOTMOUNT); then
+if ! "$GOTBUSYBOX" && ! "$GOTMOUNT"; then
 	# This should never happen, but safety check is always good
-	echo "FATAL ERROR, NO BUSYBOX NEITHER MOUNT" >> $LOG
+	echo "FATAL ERROR, NO BUSYBOX NEITHER MOUNT" >> "$LOG"
 	exit 1
 fi
 
 ADMOUNT "$1"
-FS=$(mount | grep "$1" | head -n 1 | awk '{print $5}')
+FS="$(mount | grep "$1" | head -n 1 | awk '{print $5}')"
 echo "$FS"
 
 sync