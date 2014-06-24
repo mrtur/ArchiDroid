a1e15b3f59669bee14751b63bd75c92d561f22e6
mrtur <mrturmrtur@gmail.com>
2014-06-24 08:23:56 +0200

    cm-11-20140623-NIGHTLY-mako

--- recovery/etc/install-cm-recovery.sh
+++ recovery/etc/install-cm-recovery.sh
@@ -1,11 +1,11 @@
 #!/system/bin/sh
 if [ -f /system/etc/recovery-transform.sh ]; then
-  exec sh /system/etc/recovery-transform.sh 9017344 9f7df2a33e6a8e9bdcfe12baea0bf3e5390b11b2 6684672 e05cbf9046f85e308dfc3967fa477479cd295e88
+  exec sh /system/etc/recovery-transform.sh 9017344 5aa1cc61ac43bad780bfe2c1139cd904e44d9415 6684672 36178c27b8bca6f001e0b2efbf49d8ae1f9140e2
 fi
 
-if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery:9017344:9f7df2a33e6a8e9bdcfe12baea0bf3e5390b11b2; then
+if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery:9017344:5aa1cc61ac43bad780bfe2c1139cd904e44d9415; then
   log -t recovery "Installing new recovery image"
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:6684672:e05cbf9046f85e308dfc3967fa477479cd295e88 EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery 9f7df2a33e6a8e9bdcfe12baea0bf3e5390b11b2 9017344 e05cbf9046f85e308dfc3967fa477479cd295e88:/system/recovery-from-boot.p
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:6684672:36178c27b8bca6f001e0b2efbf49d8ae1f9140e2 EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery 5aa1cc61ac43bad780bfe2c1139cd904e44d9415 9017344 36178c27b8bca6f001e0b2efbf49d8ae1f9140e2:/system/recovery-from-boot.p
 else
   log -t recovery "Recovery image already installed"
 fi
